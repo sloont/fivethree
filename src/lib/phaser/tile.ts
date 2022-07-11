7a7864b3b98ab068f1fdfe04a6e45e8f264d456b
sloont <colinabel93@gmail.com>
2022-07-10 22:18:26 -0400

    dont flame. its progress

--- src/lib/phaser/tile.ts
+++ src/lib/phaser/tile.ts
@@ -6,7 +6,22 @@ const TILESHEET: number[] = [
     0xff5301
 ];
 
-const TILE_ALPHA: number = 0.5;
+const TILE_ALPHA = 0.5;
+const TEST_ANGLE = Math.atan(0.5);
+
+function warp(angle: number /*radians*/, point: Phaser.Geom.Point, scene: Phaser.Scene): Phaser.Geom.Point {
+    const transform = [Math.cos(angle), Math.sin(angle)];
+    const origin = new Phaser.Geom.Point(0.5, 0.5);
+    
+        point.x = (point.x - point.y) * transform[0];
+        point.y = (point.x + point.y) * transform[1];
+    
+        // const { width, height } = scene.sys.game.config;
+        // point.x += Number(width) * origin.x;
+        // point.y += Number(height) * origin.y;
+    
+    return point;
+}
 
 export class Tile extends Phaser.GameObjects.Graphics {
 
@@ -32,6 +47,29 @@ export class Tile extends Phaser.GameObjects.Graphics {
         this.strokeRect(0, 0, 64, 64);
 
         this.setPosition(this.x-32, this.y-32)
+
+
+        const points: Phaser.Geom.Point[] = [
+            new Phaser.Geom.Point(-32,-32),
+            new Phaser.Geom.Point(32,-32),
+            new Phaser.Geom.Point(32,32),
+            new Phaser.Geom.Point(-32,32)
+        ];
+
+        for (let i = 0; i < points.length; i++) {
+            warp(TEST_ANGLE, points[i], scene);
+        }
+
+        console.log(points);
+        // this.lineStyle(3, 0x24252a, 1.0); => assumed
+        this.fillStyle(0xff5301, TILE_ALPHA);
+        this.fillPoints(points, true, true);
+        this.strokePoints(points, true, true);
+
+        this.setRotation(Math.PI - TEST_ANGLE)
+        
+
+        
 
         scene.add.existing(this);
     }
